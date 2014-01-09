d13385808392af85f55bbf57786427ae86251671
contessa <tessa.fallon@gmail.com>
2014-01-09 14:47:41 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amoeba::Application.config.secret_token = '2439265b0b386079f29888c1b599d44eb6ef9e42af110b7ea20c53e162a6de4932d7f35f8f3742842089f4ea577635e5b08809cf9cc8811a2d2cbd4f3b6d3c8f'
